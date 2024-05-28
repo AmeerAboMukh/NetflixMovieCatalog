31eba04f1e55a5d343a32acc70ffb2c6fb591f3c
ameerabomukh <ameerabomukh8000@gmail.com>
2024-05-28 14:05:19 +0300

    deploy error fix

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 
 cd ~/app/
 
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-fi
+# Install python3-venv package if not already installed
+sudo apt update
+sudo apt install python3-venv -y
 
+# Create and activate virtual environment
+python3 -m venv venv
 source venv/bin/activate
 
+# Install Python dependencies
 pip install -r requirements.txt
 
 #do this line to pass the automated tests
